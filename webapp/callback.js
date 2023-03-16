479caa1bc47e11c868f6455fad9f4d12c0fabd87
Davi Yamashita <116822280+DaviYamashita@users.noreply.github.com>
2023-03-16 09:35:11 -0300

    att

--- webapp/callback.js
+++ webapp/callback.js
@@ -1,6 +1,5 @@
 (() =>{
             //força repost na janela pai (passagem de signed request)
-
             window.opener.Sfdc.canvas.client.repost({refresh : true});
             window.close()
 })()
